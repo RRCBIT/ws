19be1474613b2a6d1aaaf2303268e0c85833696f
ccckmit <ccckmit@gmail.com>
2019-10-06 20:10:24 +0800

    pass test 09-blogSide

--- code/C1-fsBlog/06-blogLogin/app.js
+++ code/C1-fsBlog/06-blogLogin/app.js
@@ -78,7 +78,7 @@ function mdRender (ctx, md) { // 呈現 .md 檔案的畫面
 
 function mdEdit (ctx, md) { // 編輯 .md 檔案的畫面
   let body = (ctx.session.userId == null) ? '請先 <a href="/login">登入</a> 後才能編輯！': `
-  <form action="${ctx.path}?op=save" method="post">
+  <form id="editForm" action="${ctx.path}?op=save" method="post">
     <textarea name="mdText">${md}</textarea>
     <br/><br/>
     <button>儲存</button>
@@ -128,39 +128,42 @@ async function blogSaveFile(ctx) { // 處理 POST 儲存檔案的請求
   ctx.redirect('/blog/'+file) // 顯示儲存完成後的檔案
 }
 
-function pageLogin (ctx) { // 編輯 .md 檔案的畫面
+function pageLogin (ctx) { // 顯示登入畫面
   ctx.body = layout(ctx, `
-    <label style="color:red">${ctx.session.msg}</label>
     <form action="login" method="post">
-      <h5>帳號</h5>
-      <input name="id" type="text" value=""><br/>
-      <h5>密碼</h5>
-      <input name="password" type="password" value=""><br/><br/>
-      <button>登入</button><br/>
+      <p style="color:red">${ctx.session.msg}</p>
+      <p>
+        <label>帳號</label><br/>
+        <input name="id" type="text" value="">
+      </p>
+      <p>
+        <label>密碼</label><br/>
+        <input name="password" type="password" value="">
+      </p>
+      <button>登入</button>
     </form>
   `)
   ctx.session.msg = ''
 }
 
-const users = {
+const users = { // 預設的使用者帳號密碼
   'ccc': {password: '123'},
   'snoopy': {password: '456'}
 }
 
-async function login(ctx) {
-  let id = ctx.request.body.id
-  let password = ctx.request.body.password
-  let user = users[id]
-  if (user == null || user.password !== password) {
-    ctx.session.msg = '登入失敗，請重新輸入!'
-    ctx.redirect('/login')
+async function login(ctx) { // 登入檢查
+  let {id, password} = ctx.request.body // 取得使用者輸入的帳號密碼
+  let user = users[id] // 取得 id 對應的使用者
+  if (user != null && user.password == password) { // 如果該使用者存在且密碼正確，則登入成功
+    ctx.session.userId = id // 用 session 記住這個使用者帳號
+    ctx.redirect('/') // 導回首頁
   } else {
-    ctx.session.userId = id
-    ctx.redirect('/')
+    ctx.session.msg = '登入失敗，請重新輸入!'
+    ctx.redirect('/login') // 導回登入畫面並顯示錯誤訊息！
   }
 }
 
-async function logout(ctx) {
-  ctx.session.userId = undefined
-  ctx.redirect('/login')
+async function logout(ctx) { // 登出
+  ctx.session.userId = undefined // 從 session 中刪除該使用者 (強迫忘記該使用者)
+  ctx.redirect('/login') // 回到登入畫面
 }
